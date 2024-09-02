5815a429fceb9a76b7a5687a6061d73b8eb5cbaa
OUMAIMAtakrour <takrouroumaima@gmail.com>
2024-09-02 21:36:02 +0100

    Feat:adding scores functionalities and resetting the game

--- index.js
+++ index.js
@@ -1,130 +1,332 @@
 const game = document.querySelector("#game");
 const infos = document.querySelector("#infos");
-console.log(game);
-// const cells = [];
-// for(let i;i<400;i++){
-//     cells.push("");
-// }
+const winPopup = document.getElementById("winPopup");
+const winMessage = document.getElementById("winMessage");
+const resetBtn = document.getElementById("resetBtn");
 
 const cells = Array(400).fill("");
 const size = Math.sqrt(400);
 const toWin = 5;
-// const row = cells.length;
-// console.log(row);
 
-let count = 0;
 let play = "cross";
-infos.textContent = "cross goes first";
+let players = [];
+
+function loadPlayers() {
+  const storedPlayers = localStorage.getItem("players");
+  if (storedPlayers) {
+    players = JSON.parse(storedPlayers);
+  }
+}
+
+function addPlayer(name) {
+  const existingPlayer = players.find((player) => player.name === name);
+  if (existingPlayer) {
+    return existingPlayer;
+  } else {
+    const newPlayer = {
+      name: name,
+      score: 0,
+    };
+    players.push(newPlayer);
+    return newPlayer;
+  }
+}
+
+function updateScore(playerName) {
+  const player = players.find((p) => p.name === playerName);
+  if (player) {
+    player.score++;
+    saveGameHistory();
+    displayPlayerInfo();
+  }
+}
+
+function saveGameHistory() {
+  localStorage.setItem("players", JSON.stringify(players));
+}
+
 function createGrid() {
   cells.forEach((_cell, index) => {
     const cellElement = document.createElement("div");
     cellElement.classList.add("square");
     cellElement.id = index;
     cellElement.addEventListener("click", addGo);
-    // const circleElement = document.createElement("div");
-    // circleElement.classList.add("cross");
-    // cellElement.appendChild(circleElement);
     game.appendChild(cellElement);
   });
 }
-createGrid();
+
 function addGo(e) {
-  console.log("clicked", e.target);
   const start = document.createElement("div");
   start.classList.add(play);
+
   e.target.append(start);
+
+  const currentPlayer1 = localStorage.getItem("currentPlayer1");
+  const currentPlayer2 = localStorage.getItem("currentPlayer2");
+  const playerName = play === "cross" ? currentPlayer1 : currentPlayer2;
+
   play = play === "cross" ? "circle" : "cross";
-  console.log(play);
-  infos.textContent = play + "'s turn";
+  infos.textContent = `${
+    play === "cross" ? currentPlayer1 : currentPlayer2
+  } (${play})'s turn`;
   e.target.removeEventListener("click", addGo);
+
   win();
 }
 
-
-winningCombinations = [];
-
-function win(e) {
+function win() {
   const allSquares = document.querySelectorAll(".square");
+  const winnerName =
+    play === "cross"
+      ? localStorage.getItem("currentPlayer1")
+      : localStorage.getItem("currentPlayer2");
+  const winner = players.find((player) => player.name === winnerName);
+
   function removeListeners() {
     allSquares.forEach((square) => {
       square.removeEventListener("click", addGo);
     });
   }
+
   for (let i = 0; i < allSquares.length; i += size) {
-    for (let j = 0; j < 16; j++) {
-      const slice = Array.from(allSquares).slice(i + j, i + j + 5);
+    for (let j = 0; j <= size - toWin; j++) {
+      const slice = Array.from(allSquares).slice(i + j, i + j + toWin);
       if (
-        (slice.length === 5 &&
-          slice.every((square) =>
-            square.firstChild?.classList.contains("cross")
-          )) ||
-        (slice.length === 5 &&
-          slice.every((square) =>
-            square.firstChild?.classList.contains("circle")
-          ))
-      ) {
-        infos.textContent = "win";
-        removeListeners();
-        return;
-      }
-    }
-
-    for (let i = 0; i <= allSquares.length - toWin * (size + 1); i++) {
-      let leftToRight = [];
-      for (let j = 0; j < toWin; j++) {
-        leftToRight.push(allSquares[i + j * (size + 1)]);
-      }
-
-      if (
-        leftToRight.length === toWin &&
-        leftToRight.every((square) =>
+        slice.every((square) =>
           square.firstChild?.classList.contains("cross")
-        )
+        ) ||
+        slice.every((square) => square.firstChild?.classList.contains("circle"))
       ) {
-        infos.textContent = "Cross wins!";
+        winMessage.textContent = `${winner.name} wins!`;
+        winPopup.classList.remove("hidden");
         removeListeners();
-        return;
-      }
-
-      if (
-        leftToRight.length === toWin &&
-        leftToRight.every((square) =>
-          square.firstChild?.classList.contains("circle")
-        )
-      ) {
-        infos.textContent = "Circle wins!";
-        removeListeners();
-        return;
-      }
-    }
-
-    for (let i = toWin - 1; i <= allSquares.length - toWin * size; i++) {
-      let rightToLeft = [];
-      for (let j = 0; j < toWin; j++) {
-        rightToLeft.push(allSquares[i + j * (size - 1)]);
-      }
-
-      if (
-        rightToLeft.length === toWin &&
-        rightToLeft.every((square) =>
-          square.firstChild?.classList.contains("cross")
-        )
-      ) {
-        infos.textContent = "Cross wins!";
-        removeListeners();
-        return;
-      }
-
-      if (
-        rightToLeft.length === toWin &&
-        rightToLeft.every((square) =>
-          square.firstChild?.classList.contains("circle")
-        )
-      ) {
-        infos.textContent = "Circle wins!";
-        removeListeners();
+        resetBtn.addEventListener("click", resetGame);
+        updateScore(winner.name);
         return;
       }
     }
   }
+
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j <= size - toWin; j++) {
+      let slice = [];
+      for (let k = 0; k < toWin; k++) {
+        slice.push(allSquares[i + k * size + j * size]);
+      }
+      if (
+        slice.every((square) =>
+          square.firstChild?.classList.contains("cross")
+        ) ||
+        slice.every((square) => square.firstChild?.classList.contains("circle"))
+      ) {
+        winMessage.textContent = `${winner.name} wins!`;
+        winPopup.classList.remove("hidden");
+        removeListeners();
+        resetBtn.addEventListener("click", resetGame);
+        updateScore(winner.name);
+        return;
+      }
+    }
+  }
+
+  for (let i = 0; i <= allSquares.length - toWin * (size + 1); i++) {
+    let leftToRight = [];
+    for (let j = 0; j < toWin; j++) {
+      leftToRight.push(allSquares[i + j * (size + 1)]);
+    }
+
+    if (
+      leftToRight.every((square) =>
+        square.firstChild?.classList.contains("cross")
+      ) ||
+      leftToRight.every((square) =>
+        square.firstChild?.classList.contains("circle")
+      )
+    ) {
+      winMessage.textContent = `${winner.name} wins!`;
+      winPopup.classList.remove("hidden");
+      removeListeners();
+      resetBtn.addEventListener("click", resetGame);
+      updateScore(winner.name);
+      return;
+    }
+  }
+
+  for (let i = toWin - 1; i <= allSquares.length - toWin * size; i++) {
+    let rightToLeft = [];
+    for (let j = 0; j < toWin; j++) {
+      rightToLeft.push(allSquares[i + j * (size - 1)]);
+    }
+
+    if (
+      rightToLeft.every((square) =>
+        square.firstChild?.classList.contains("cross")
+      ) ||
+      rightToLeft.every((square) =>
+        square.firstChild?.classList.contains("circle")
+      )
+    ) {
+      winMessage.textContent = `${winner.name} wins!`;
+      winPopup.classList.remove("hidden");
+      removeListeners();
+      resetBtn.addEventListener("click", resetGame);
+      updateScore(winner.name);
+      return;
+    }
+  }
 }
+
+function resetGame() {
+  const allSquares = document.querySelectorAll(".square");
+
+  allSquares.forEach((square) => {
+    while (square.firstChild) {
+      square.removeChild(square.firstChild);
+    }
+  });
+
+  play = "cross";
+  const currentPlayer1 = localStorage.getItem("currentPlayer1");
+  infos.textContent = `${currentPlayer1} (Cross) goes first`;
+  cells.fill("");
+  winPopup.classList.add("hidden");
+  game.innerHTML = "";
+  createGrid();
+  displayPlayerInfo();
+}
+
+const startButton = document.querySelector("#startGame");
+
+startButton.addEventListener("click", () => {
+  const player1Name =
+    document.querySelector("#player1").value.trim() || "Player 1";
+  const player2Name =
+    document.querySelector("#player2").value.trim() || "Player 2";
+
+  loadPlayers();
+  const player1 = addPlayer(player1Name);
+  const player2 = addPlayer(player2Name);
+
+  localStorage.setItem("currentPlayer1", player1.name);
+  localStorage.setItem("currentPlayer2", player2.name);
+
+  saveGameHistory();
+  displayPlayerInfo();
+  window.location.href = "game.html";
+});
+
+function displayPlayerInfo() {
+  loadPlayers();
+  const currentPlayer1 = localStorage.getItem("currentPlayer1");
+  const currentPlayer2 = localStorage.getItem("currentPlayer2");
+
+  const player1 = players.find((player) => player.name === currentPlayer1) || {
+    name: "Player 1",
+    score: 0,
+  };
+  const player2 = players.find((player) => player.name === currentPlayer2) || {
+    name: "Player 2",
+    score: 0,
+  };
+
+  const infoContainer = document.querySelector("#playerInfo");
+  if (infoContainer) {
+    infoContainer.innerHTML = `
+      <div>${player1.name}: ${player1.score}</div>
+      <div>${player2.name}: ${player2.score}</div>
+    `;
+  }
+
+  infos.textContent = `${player1.name} (Cross) goes first`;
+}
+
+function showGameHistory() {
+  loadPlayers();
+  const historyContainer = document.querySelector("#history");
+  historyContainer.innerHTML = "";
+
+  players.forEach((player) => {
+    const historyItem = document.createElement("div");
+    historyItem.textContent = `${player.name}  Score : ${player.score}`;
+    historyContainer.appendChild(historyItem);
+  });
+}
+
+function clearGameHistory() {
+  localStorage.removeItem("players");
+  players = [];
+  document.querySelector("#history").innerHTML = "";
+  displayPlayerInfo();
+}
+
+document
+  .querySelector("#showHistory")
+  .addEventListener("click", showGameHistory);
+
+const clearHistoryButton = document.querySelector("#clearHistory");
+if (clearHistoryButton) {
+  clearHistoryButton.addEventListener("click", clearGameHistory);
+}
+
+loadPlayers();
+createGrid();
+displayPlayerInfo();
+
+const scores = [
+  { username: "Player1", score: 500 },
+  { username: "Player2", score: 450 },
+  { username: "Player3", score: 400 },
+  { username: "Player4", score: 350 },
+  { username: "Player5", score: 300 },
+];
+
+const popup = document.getElementById("scorePopup");
+
+const showScoresBtn = document.getElementById("showScores");
+const closeBtn = document.getElementsByClassName("close")[0];
+const scoresList = document.getElementById("scoresList");
+
+function populateScores() {
+  const players = JSON.parse(localStorage.getItem("players")) || [];
+  scoresList.innerHTML = "";
+
+  players.forEach((player) => {
+    const scoreItem = document.createElement("div");
+    scoreItem.className = "score-item";
+    scoreItem.innerHTML = `
+          <span class="username">${player.name}</span>
+          <span class="score">${player.score} pts</span>
+      `;
+    scoresList.appendChild(scoreItem);
+  });
+}
+
+showScoresBtn.onclick = function () {
+  populateScores();
+  popup.style.display = "block";
+};
+
+closeBtn.onclick = function () {
+  popup.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == popup) {
+    popup.style.display = "none";
+  }
+};
+
+showScoresBtn.onclick = function () {
+  populateScores();
+  popup.style.display = "block";
+};
+
+closeBtn.onclick = function () {
+  popup.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == popup) {
+    popup.style.display = "none";
+  }
+};
